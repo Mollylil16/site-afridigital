20bb9d7ccfca7193101c8b398e4c2c09bfb43508
mollylil16 <brunellomepieu01@gmail.com>
2026-06-11 19:32:36 +0100

    chore: configuration pour le deploiement automatique sur GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Base path prefix for GitHub Pages default subfolder hosting.
+  // Note: Remove this or set it to empty if you link a custom domain (e.g. afridigital.ci)
+  basePath: isProd ? "/site-afridigital" : "",
+};
 
 export default nextConfig;
